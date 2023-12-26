1002c3d01bc9feaae487d1573fb5fdc3bf4e1a3a
debugmanoj <manojkumarwork01@gmail.com>
2023-12-26 18:01:14 +0530

    Completed Update and deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import TopBar from './Components/TopBar'
+import Cards from './Components/Cards'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [todo,setTodo]=useState([
+    {
+    id:1,
+    title:" Office Task-1",
+    description:"this is the description for my first task",
+    status:false
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  },
+  {
+    id:2,
+    title:" Office Task-2",
+    description:"this is the description for my second task",
+    status:false
+    
+  },
+  {
+    id:3,
+    title:" Office Task-3",
+    description:"this is the description for my third task",
+    status:false
+    
+  }
+])
+let [completed,setCompleted]=useState("All")
+  return <>
+  <TopBar todo={todo} setTodo={setTodo} completed={completed} setCompleted={setCompleted}/>
+  </>
 }
 
 export default App
